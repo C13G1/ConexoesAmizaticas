558f81624bab9163cbeded007928b2d4b40e1a08
Jonas Melo <jonasFNMelo@gmail.com>
2026-05-19 14:31:38 -0300

    fix: models unicaficated

--- ConexoesAmizaticas/Model/UserModel.swift
+++ ConexoesAmizaticas/Model/UserModel.swift
@@ -6,44 +6,29 @@
 //
 import SwiftData
 import SwiftUI
-
+ 
 @Model
-class UserModel: Codable {
-    private var name: String
-    private var profilePicture: Data
-    public var id: UUID
+class User: Codable {
+    private var name:         String
+    private var profileImage: Data
+    public  var id:           UUID
     
-    init(name: String = "DaultName", profilePicture: Data = Data()) {
-        self.name = name
-        self.profilePicture = profilePicture
-        self.id = UUID()
+    init(name: String = "DefaultName", profilePicture: Data = Data()) {
+        self.name         = name
+        self.profileImage = profilePicture
+        self.id           = UUID()
     }
     
     required init(from decoder: any Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let container     = try decoder.container(keyedBy: CodingKeys.self)
         
-        self.name = try container.decode(String.self, forKey: .name)
-        self.profilePicture = try container.decode(Data.self, forKey: .profilePicture)
-        self.id = try container.decode(UUID.self, forKey: .id)
+        self.name         = try container.decode(String.self, forKey: .name)
+        self.profileImage = try container.decode(Data.self, forKey: .profilePicture)
+        self.id           = try container.decode(UUID.self, forKey: .id)
     }
     
-    func encode(to encoder: any Encoder) throws {}
-
-}
- 
-class User {
-    private var name: String
-    private var profilePicture: Image
-    public var id: UUID
-    
-    init(name: String = "DefaultName", profilePicture: Image = Image("defaultPicture")) {
-        self.name = name
-        self.profilePicture = profilePicture
-        self.id = UUID()
-    }
-    
-    func editProfilePicture(_ image: Image) {
-        self.profilePicture = image
+    func editProfileImageData(_ image: Data) {
+        self.profileImage = image
     }
     
     func editName(_ name: String) {
@@ -53,4 +38,7 @@ class User {
     func getID() -> UUID{
         return id
     }
+    
+    func encode(to encoder: any Encoder) throws {}
 }
+
