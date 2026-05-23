ece8e956ef4a7ff793edd77eaa2356ba0ee27e9e
Dayo Araujo <dayo.araujosz@gmail.com>
2026-05-23 17:42:00 -0300

    refactor: gallery view

--- ConexoesAmizaticas/Views/Components/GalleryFrame.swift
+++ ConexoesAmizaticas/Views/Components/GalleryFrame.swift
@@ -8,28 +8,31 @@
 import SwiftUI
 
 struct GalleryFrame: View {
-    @Binding var user: User
+    let imageData: Data
     
     var body: some View {
-        VStack {
-            RoundedRectangle(cornerRadius: 20)
-                .strokeBorder(Color.white, lineWidth: 4)
-                .frame(width: UIScreen.main.bounds.width * 0.47, height: UIScreen.main.bounds.height * 0.258)
+        if let uiImage = UIImage(data: imageData) {
+            Image(uiImage: uiImage)
+                .resizable()
+                .scaledToFill()
+                .frame(width: UIScreen.main.bounds.width * 0.45,
+                       height: UIScreen.main.bounds.height * 0.25)
+                .clipShape(RoundedRectangle(cornerRadius: 16))
                 .overlay(
-                    Image(uiImage: UIImage(data: user.profilePicture)!)
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: UIScreen.main.bounds.width * 0.45, height: UIScreen.main.bounds.height * 0.25)
-                        .clipShape(RoundedRectangle(cornerRadius: 16))
+                    RoundedRectangle(cornerRadius: 16)
+                        .strokeBorder(Color.white, lineWidth: 4)
                 )
+        } else {
+            RoundedRectangle(cornerRadius: 16)
+                .frame(width: UIScreen.main.bounds.width * 0.45, height: UIScreen.main.bounds.height * 0.25)
+                .foregroundColor(.gray)
         }
     }
 }
 
 #Preview {
     let mockImage = UIImage(named: "gallery")!
-    let mockImageData = mockImage.jpegData(compressionQuality: 1)!
-    
-    let mockUser = User(profilePicture: mockImageData)
-    GalleryFrame(user: .constant(mockUser))
+    let mockData = mockImage.pngData() ?? Data()
+    return GalleryFrame(imageData: mockData)
+        .preferredColorScheme(.dark)
 }
